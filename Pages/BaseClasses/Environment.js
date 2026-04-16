2c26c1be9316b861afcc1e7eb7415c07242fdbb6
manoj.vyavahare <manoj.vyavahare@riomed.com>
2026-04-16 13:54:43 +0530

    remo space

--- Pages/BaseClasses/Environment.js
+++ Pages/BaseClasses/Environment.js
@@ -9,9 +9,7 @@ export default class Environment{
         //this.Test="http://10.0.0.63:3000/cellmaUser/login";
         //this.Test="https://10.0.0.63/cellmaUser/login"
        // this.Test1="https://10.0.0.99/cellmaUserAPI/swagger-ui/index.html"
-       // this.Test="https://10.0.0.63/cellmaUser/login"
-       
-       
+       // this.Test="https://10.0.0.63/cellmaUser/login"            
       // this.RefPortal="http://10.0.0.106:8080/cellmaPortal/portal/login"
     }     
 }
